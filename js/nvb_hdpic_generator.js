7e80282ce29da0fa19b4a0f6c33df83624a7b4e5
puppycat0109 <white199619@gmail.com>
2021-07-26 00:45:40 +0800

    [Naver]新增功能:重新載入, 刪除圖片 其他:新增FontAwesome

--- js/nvb_hdpic_generator.js
+++ js/nvb_hdpic_generator.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-    
-    let $input = $("#addresses");
 
+    let $input = $("#addresses");
+    let count = 0;
     /**重置*/
     function reset() {
 
-//        $('input[type="text"], textarea').val("");
-//        $('.result-area').css("display", "none");
-//        $('.output-link').prop('href', "");
-//        $('#files-type').val("webp");
-//        $('#files-size').val("maxres");
+        //        $('input[type="text"], textarea').val("");
+        //        $('.result-area').css("display", "none");
+        //        $('.output-link').prop('href', "");
+        //        $('#files-type').val("webp");
+        //        $('#files-size').val("maxres");
     }
 
     /**RESET BTN*/
@@ -23,7 +23,7 @@ $(document).ready(function () {
             $(this).removeAttr("data-selected-all");
         }
     });
-    
+
     $("input, textarea").click(function () {
         if (!$(this).attr("data-selected-all")) {
             try {
@@ -38,8 +38,8 @@ $(document).ready(function () {
             }
         }
     });
-    
-    
+
+
     /*---------------------*/
     /**取得封面圖片網址*/
     function getImgURLs(URL) {
@@ -51,23 +51,84 @@ $(document).ready(function () {
         let url_ary;
         let result = "";
         let img_list = "";
-        url_ary = $input.val().split('\n');
+        let resURL = "";
+        count = 0;
+        
+        url_ary = $input.val().split('\n').filter(function (el) {
+          return el != "";
+        });
         console.log("url_ary: ", url_ary);
-        
-        for(var i = 0 ; i < url_ary.length ; i++){
-            
-            img_list += "<img src='" + url_ary[i].split('?type=')[0] + "' class='m-1' style='width: 200px;' /> ";
-            
-            result += url_ary[i].split('?type=')[0] + "\r\n";
-            $("#result_count").text("總計: "+(i+1)+" 張");
+
+        for (var i = 0; i < url_ary.length; i++) {
+            count = i + 1;
+            resURL = url_ary[i].split('?type=')[0];
+
+            img_list += "<div class='imgdiv m-1' id='img_" + i + "' >";
+            img_list += "   <img src='" + resURL + "' class='image' />";
+            img_list += "   <div class='topright'>"
+            img_list += "       <div class='deleteimg' id='del_" + i + "'><i class='fas fa-times'></i></div>"
+            img_list += "   </div>"
+            img_list += "</div>"
+
+            result += resURL + "\r\n";
+
+
         }
-        
         $('#output').val(result);
+        $("#result_count").text("總計: " + count + " 張");
         $('#output_img').html(img_list);
+
+        loadDeleteFun();
+    }
+
+    function loadImgFromResult() {
+
+        let url_ary = $("#output").val().split('\n').filter(function (el) {
+          return el != "";
+        });
+        let img_list = "";
+        let resURL = "";
+        count = 0;
         
+        console.log("url_ary: ", url_ary);
+
+        for (var i = 0; i < url_ary.length; i++) {
+            count = i + 1;
+            resURL = url_ary[i];
+            img_list += "<div class='imgdiv m-1' id='img_" + i + "' >";
+            img_list += "   <img src='" + resURL + "' class='image' />";
+            img_list += "   <div class='topright'>"
+            img_list += "       <div class='deleteimg' id='del_" + i + "'><i class='fas fa-times'></i></div>"
+            img_list += "   </div>"
+            img_list += "</div>"
+        }
+        $("#result_count").text("總計: " + count + " 張");
+        $('#output_img').html(img_list);
+        loadDeleteFun();
+    }
+    
+    function loadDeleteFun(){
+        $(".deleteimg").on('click', function () {
+            let idnum = $(this).attr("id").split('_')[1];
+            $("#img_" + idnum).remove();
+            count = count - 1;
+            $("#result_count").text("總計: " + count + " 張");
+        });
     }
 
     $('#ok').on('click', function () {
         getImgURLs($input.val());
     });
+
+    $('#reset').on('click', function () {
+        getImgURLs($input.val());
+    });
+    
+    $('#reload').on('click', function () {
+        loadImgFromResult();
+    });
+
+
+
+
 });
